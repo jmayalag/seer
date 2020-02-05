7316850f2dbe9f57a21b400c046eb98d2a11a1c2
Jordan Ayala <jor.ayalag@gmail.com>
2020-02-05 04:16:32 +0000

    Test combined strategy

--- analysis/combined_strategy.R
+++ analysis/combined_strategy.R
@@ -4,166 +4,111 @@ library(dplyr)
 library(seer)
 library(quantstrat)
 
-#' Conservative strategy
-#'
-#' el dataset requiere las columnas
-#' - ind_enter, ind_exit: Indicadores tecnicos
-#' - pred_bull, pred_bear: Indicadores predictivos
-#'
-#' Enter: prediction & signal
-#' @param fees costo numerico o funcion de costo
-#'
-#' @return un objeto Strategy
-#' @export
-#' @importFrom quantstrat strategy add.indicator add.rule add.signal
-conservative_strat <- function(fees = 0) {
-  # Order params
-  order_size <- 100
-  
-  strat <- strategy(paste("conservative_ml", sep = "_"))
-  
+conservative_signals <- function(strat) {
   strat <- add.signal(
     strategy = strat,
     name = "sigFormula",
     arguments = list(
-      formula = "ind_enter & pred_bull",
+      formula = "enterLong & pred_bull",
       cross = TRUE
     ),
-    label = "sig_long_enter"
+    label = "enterLongPred"
   )
   
   strat <- add.signal(
     strategy = strat,
     name = "sigFormula",
     arguments = list(
-      formula = "ind_exit | pred_bear",
+      formula = "exitLong | pred_bear",
       cross = TRUE
     ),
-    label = "sig_long_exit"
-  )
-  
-  strat <- add.rule(
-    strategy = strat,
-    name = "ruleSignal",
-    arguments = list(
-      sigcol = "sig_long_enter",
-      sigval = TRUE,
-      orderqty = order_size,
-      ordertype = "market",
-      orderside = "long",
-      prefer = "Open",
-      TxnFees = fees,
-      replace = FALSE
-    ),
-    type = "enter",
-    label = "rule_enter_long"
-  )
-  
-  strat <- add.rule(
-    strategy = strat,
-    name = "ruleSignal",
-    arguments = list(
-      sigcol = "sig_long_exit",
-      sigval = TRUE,
-      orderside = "long",
-      ordertype = "market",
-      orderqty = "all",
-      TxnFees = fees,
-      replace = TRUE
-    ),
-    type = "exit",
-    label = "rule_exit_long"
+    label = "exitLongPred"
   )
   
   strat
 }
 
-#' Risky strategy
+risky_signals <- function(strat) {
+  strat <- add.signal(
+    strategy = strat,
+    name = "sigFormula",
+    arguments = list(
+      formula = "enterLong | pred_bull",
+      cross = TRUE
+    ),
+    label = "enterLongPred"
+  )
+  
+  strat <- add.signal(
+    strategy = strat,
+    name = "sigFormula",
+    arguments = list(
+      formula = "exitLong & pred_bear",
+      cross = TRUE
+    ),
+    label = "exitLongPred"
+  )
+  
+  strat
+}
+
+
+#' ML strategy
 #'
 #' el dataset requiere las columnas
-#' - ind_enter, ind_exit: Indicadores tecnicos
+#' - enterLong, exitLong: Indicadores tecnicos
 #' - pred_bull, pred_bear: Indicadores predictivos
 #'
 #' Enter: prediction & signal
+#' @param base_strategy nombre de la estrategia de analisis tecnico
+#' @param approach debe existir la funcion [approach]_signals
 #' @param fees costo numerico o funcion de costo
 #'
 #' @return un objeto Strategy
 #' @export
 #' @importFrom quantstrat strategy add.indicator add.rule add.signal
-risky_strat <- function(base_strategy, fees = 0) {
+ml_strat <- function(base_strategy, approach, fees = 0) {
   # Order params
   order_size <- 100
-  
-  strat <- strategy(paste("risky_ml", sep = "_"))
-  
-  strat <- add.signal(
-    strategy = strat,
-    name = "sigFormula",
-    arguments = list(
-      formula = "ind_enter & pred_bull",
-      cross = TRUE
-    ),
-    label = "sig_long_enter"
-  )
-  
-  strat <- add.signal(
-    strategy = strat,
-    name = "sigFormula",
-    arguments = list(
-      formula = "ind_exit | pred_bear",
-      cross = TRUE
-    ),
-    label = "sig_long_exit"
-  )
-  
+
+  strat <- strategy(paste(approach, "ml", base_strategy, sep = "_"))
+  approach_fun <- get(paste0(approach, "_signals"))
+
+  strat <- approach_fun(strat)
+
   strat <- add.rule(
     strategy = strat,
     name = "ruleSignal",
-    arguments = list(
-      sigcol = "sig_long_enter",
+    arguments = default_rule_arguments(
+      order_size = order_size,
+      fees = fees,
+      sigcol = "enterLongPred",
       sigval = TRUE,
-      orderqty = order_size,
-      ordertype = "market",
       orderside = "long",
-      prefer = "Open",
-      TxnFees = fees,
       replace = FALSE
     ),
     type = "enter",
-    label = "rule_enter_long"
+    label = "rule_enterLongPred"
   )
-  
-  strat <- add.rule(
-    strategy = strat,
-    name = "ruleSignal",
-    arguments = list(
-      sigcol = "sig_long_exit",
-      sigval = TRUE,
-      orderside = "long",
-      ordertype = "market",
-      orderqty = "all",
-      TxnFees = fees,
-      replace = TRUE
-    ),
-    type = "exit",
-    label = "rule_exit_long"
-  )
+
+  strat <- add_exit_rule(strategy = strat, fees = fees, sigcol = "exitLongPred")
+
+  strat
 }
 
 order_size <- 100
 fees <- 0
 
 set.seed(42)
-mkt <- read_ohlcv("~/datasets/datasets_paper/ES35_D1.csv") %>% head(15)
+mkt <- read_ohlcv("~/datasets/datasets_paper/ES35_D1.csv")
+base_strategy <- macd()
+mkt <- seer::apply_indicator_signals(base_strategy, mkt)
 mkt$pred_bear <- runif(nrow(mkt)) < 0.5
 mkt$pred_bull <- runif(nrow(mkt)) > 0.5
-mkt$ind_enter <- runif(nrow(mkt)) > 0.5
-mkt$ind_exit <- runif(nrow(mkt)) > 0.8
 ES35_D1 <- mkt
 
+strat <- ml_strat("macd", approach = "conservative")
+result <- backtest.opt(strat, "ES35_D1")
 
-strat <- conservative_strat()
-  
-indi <- applyIndicators(strat, mktdata = mkt)
-sign <- applySignals(strat, mkt, indicators = indi)
+strat <- ml_strat("macd", approach = "risky")
 result <- backtest.opt(strat, "ES35_D1")
